153cebe94e2562420cc22026a2925586cc8a5515
ivana <ivana.kruk@gmail.com>
2021-10-11 12:19:09 -0300

    enviando letra

--- ahorcado/src/components/CallGet/CallGet.js
+++ ahorcado/src/components/CallGet/CallGet.js
@@ -5,7 +5,10 @@ import React from 'react'
 const CallGet = () => {
 
     const baseURl = 'https://back-sandbox.herokuapp.com/api';
- 
+    const [letra,setLetra] = useState('');
+    const onChangeLetter = (e) => {
+        setLetra(e.target.value);
+    }
 
     const iniciarJuego = async () =>{
         
@@ -14,24 +17,46 @@ const CallGet = () => {
                 method:'GET',
                 headers: {
                     'Content-Type': 'application/json',
-                    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNWY3OTQxNGVmYmQ2MDAwNDg2ZWQ1NyIsImlhdCI6MTYzMzkwOTQ1Nn0.ewNIsqq0w5Y7P9e0y21MtDDNlqoLvwNptfQHIUTeWVI"
+                    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNWY3OTQxNGVmYmQ2MDAwNDg2ZWQ1NyIsImlhdCI6MTYzMzk2NDYzMX0.RlKAAFAa1OGvwIqhHTTTp_tAq1gxyohDj_pbw3FMRzE"
                 }
             })
             const json = await response.json();
-            console.log(json);
+            console.log(json.data);
         } 
         catch (error) {
             alert(error)
         }
     }
 
+    const enviarLetra = async () =>
+    
+    {
+        try {
+            const response = await fetch (`https://back-sandbox.herokuapp.com/api/hanged-game/try`,
+            {
+                method:'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNWY3OTQxNGVmYmQ2MDAwNDg2ZWQ1NyIsImlhdCI6MTYzMzk2NDYzMX0.RlKAAFAa1OGvwIqhHTTTp_tAq1gxyohDj_pbw3FMRzE"
+                },
+                body: JSON.stringify({"text": letra})
+            })
+            const json = await response.json();
+            console.log(json);
+         
+        } catch (error) {
+            alert(`${error} error letra`);
+        }
+
+
+    }
+
     return (
         <div>
             <button onClick={iniciarJuego}>Iniciar juego</button>
-         {/*    <input type="text" onChange={}/>
-            <button onClick={}>Enviar letra</button>
-            <h1>{}</h1>
-            <h1>{}</h1> */}
+            <input type="text" onChange={onChangeLetter} />
+            <button onClick={enviarLetra}>Enviar letra</button>
+           
         </div>
     )
 }
